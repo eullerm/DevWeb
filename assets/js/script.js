ea25442367eacc82c00e32b6699adc3be4f477d1
Euller Macena <euller_macena@id.uff.br>
2020-09-28 15:04:30 -0300

    Trabalho 1 finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-$(function() {
+$(function () {
    $('[data-toggle="tooltip"]').tooltip();
    $('[data-toggle="popover"]').popover();
 })
+
+$(document).ready(function () {
+   $(".show-toast").click(function () {
+      $("#myToast").toast({ delay: 3000 });
+      $("#myToast").toast('show');;
+   });
+});
+
+$('#myModal').on('shown.bs.modal', function () {
+   $('#myInput').trigger('focus')
+ })
+
+var $range = document.querySelector('input'),
+    $value = document.querySelector('span');
+
+$range.addEventListener('input', function() {
+  $value.textContent = this.value;
+});
